725c6cddc108bc5c495b1c4488903242db7f3732
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-09-22 22:51:49 +0300

    Initial Commit

---
+++ DatePickerInSwiftUI/DatePickerInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerInSwiftUIApp.swift
+//  DatePickerInSwiftUI
+//
+//  Created by Ramill Ibragimov on 22.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
